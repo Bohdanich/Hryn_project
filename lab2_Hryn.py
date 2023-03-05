761a7454aca9220a05d88c5ec1b22478b6df4f14
Bohdan <bohdan.hryn2002@gmail.com>
2023-03-05 19:42:22 +0200

    Lab2

---
+++ lab2_Hryn.py
@@ -0,0 +1,12 @@
+print("Programming", "Essential", "in", sep='***', end='...')
+print("Python\n")
+
+print("    *    \n   * *   \n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n")
+
+print("I'm student\n")
+
+print('"I\'m" \n""learning"" \n"""Python"""\n')
+
+print(0o500)
+
+print(0x777)
